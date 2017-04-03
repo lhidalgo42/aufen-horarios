f250e6ed107c8953706bd51f74d8e1fcdbe79bf7
lhidalgo42 <lhidalgosep@gmail.com>
2017-04-03 09:14:11 -0300

    filtro de horario

--- app/controllers/RoomsController.php
+++ app/controllers/RoomsController.php
@@ -9,14 +9,17 @@ class RoomsController extends \BaseController {
     {
         ExchangeController::find();
         $room = strtoupper($room);
+        $schedules = Schedule::where('start', '>=', date('Y-m-d 09:00:00'))->where('active',1)->where('end', '<=', date('Y-m-d 19:00:00'))->where('room', $room)->get();
         $events = [];
-        foreach ($schedules as $schedule) {
-            $events[] = Calendar::event(
-                $schedule->name,
-                false,
-                new DateTime($schedule->start),
-                new DateTime($schedule->end)
-            );
+        if(count($schedules)>0) {
+            foreach ($schedules as $schedule) {
+                $events[] = Calendar::event(
+                    $schedule->name,
+                    false,
+                    new DateTime($schedule->start),
+                    new DateTime($schedule->end)
+                );
+            }
         }
 
         $calendar = Calendar::addEvents($events)
