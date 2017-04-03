3b790184978b9abd954d7b175b1c9edb73310bfa
lhidalgo42 <lhidalgosep@gmail.com>
2017-04-03 17:47:04 -0300

    cambios

--- app/views/pages/room.blade.php
+++ app/views/pages/room.blade.php
@@ -50,17 +50,17 @@
         }
     </style>
 </head>
-<body onload="updateClock(); setInterval('updateClock()', 1000 )">
+<body>
 <div class="col-md-12">
     <div class="col-sm-9">
         <img src="/img/logo.JPG" alt="" style="width: 100%">
     </div>
     <div class="col-sm-3">
         <div class="col-md-12">
-            <span id="clock" style="width: 100%;font-size: 70px">&nbsp;</span>
+            <span id="clock" style="width: 100%;font-size: 60px">{{date('h:i A')}}</span>
         </div>
         <div class="col-md-12">
-            <span id="date" style="width: 100%;font-size: 42px">&nbsp;</span>
+            <span id="date" style="width: 100%;font-size: 35px">{{date('d-m-Y')}}</span>
         </div>
     </div>
 </div>
@@ -71,40 +71,5 @@
     {{ $calendar->calendar() }}
     {{ $calendar->script() }}
 </div>
-<script>
-    function updateClock() {
-        var currentTime = new Date();
-
-        var currentHours = currentTime.getHours();
-        var currentMinutes = currentTime.getMinutes();
-        // Pad the minutes and seconds with leading zeros, if required
-        currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-        // Convert an hours component of "0" to "12"
-        currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-        // Choose either "AM" or "PM" as appropriate
-        var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-        // Compose the string for display
-        // Convert the hours component to 12-hour format if needed
-        currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-        var currentTimeString = currentHours + ":" + currentMinutes+ ":"+ timeOfDay;
-        // Update the time display
-        document.getElementById("clock").firstChild.nodeValue = currentTimeString;
-    }
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-
-    var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd;
-    }
-    if(mm<10){
-        mm='0'+mm;
-    }
-    var today = dd+'-'+mm+'-'+yyyy;
-    document.getElementById("date").firstChild.nodeValue = today;
-
-    // -->
-</script>
 </body>
 </html>
